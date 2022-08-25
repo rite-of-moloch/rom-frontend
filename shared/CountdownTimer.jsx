588c3fa78d0a826654cee54888cd39411285cc4e
Coltrane Yan <coltraneyan@gmail.com>
2022-08-25 12:50:21 -0400

    styling + component ideas

--- shared/CountdownTimer.jsx
+++ shared/CountdownTimer.jsx
@@ -1,7 +1,7 @@
-import React from 'react';
-import { useCountdown } from '../hooks/useCountdown';
-import { Box, Flex, Text, Image, Heading } from '@chakra-ui/react';
-import { sixMonthsInSeconds } from '../utils/constants';
+import React from "react";
+import { useCountdown } from "../hooks/useCountdown";
+import { Box, Flex, Text, Image, Heading } from "@chakra-ui/react";
+import { sixMonthsInSeconds } from "../utils/constants";
 
 export const CountdownTimer = ({ targetDate }) => {
   const [days, hours, minutes, seconds, secondsLeft] = useCountdown(targetDate);
@@ -19,7 +19,6 @@ export const CountdownTimer = ({ targetDate }) => {
         hours={hours}
         minutes={minutes}
         seconds={seconds}
-        color={color}
       />
     );
   }
@@ -28,63 +27,48 @@ export const CountdownTimer = ({ targetDate }) => {
 const ExpiredNotice = () => {
   return (
     <Flex my={6}>
-      <Heading color='red.500'>
+      <Heading color="red.500" fontFamily={"rubik"}>
         You have been sacrificed to the great Moloch.
       </Heading>
     </Flex>
   );
 };
 
-const ShowCounter = ({ days, hours, minutes, seconds, color }) => {
+const ShowCounter = ({ days, hours, minutes, seconds }) => {
   return (
     <Flex
-      fontSize='xxx-large'
-      alignItems='center'
-      justifyContent='space-between'
+      fontSize="medium"
+      alignItems="center"
+      justifyContent="space-between"
+      gap={3}
+      fontFamily={"spaceMono"}
+      color={"red"}
       py={3}
-      bgColor='rgb(71 52 89 / 16%)'
-      w='full'
+      bgColor="black"
+      w="small"
+      padding={"0.75rem"}
+      borderRadius={"5px"}
       mt={6}
     >
       {/* <Image src='/assets/citipati.png' alt='citipati' w={16} opacity={0.13} /> */}
-      <Image src='/assets/hourglass.png' alt='citipati' w={16} opacity={0.13} />
+      <Image src="/assets/hourglass.png" alt="citipati" w={5} />
       <Flex gap={6}>
-        <DateTimeDisplay
-          value={days}
-          type='D'
-          isDanger={days <= 3}
-          color={color}
-        />
-        <DateTimeDisplay
-          value={hours}
-          type='H'
-          isDanger={false}
-          color={color}
-        />
-        <DateTimeDisplay
-          value={minutes}
-          type='M'
-          isDanger={false}
-          color={color}
-        />
-        <DateTimeDisplay
-          value={seconds}
-          type='S'
-          isDanger={false}
-          color={color}
-        />
+        <DateTimeDisplay value={days} type="D" isDanger={days <= 3} />
+        <DateTimeDisplay value={hours} type="H" isDanger={false} />
+        <DateTimeDisplay value={minutes} type="M" isDanger={false} />
+        <DateTimeDisplay value={seconds} type="S" isDanger={false} />
       </Flex>
-      <Image src='/assets/scythe.png' alt='citipati' w={16} opacity={0.13} />
+      <Image src="/assets/hourglass.png" alt="citipati" w={5} />
       {/* <Image src='/assets/triskele.png' alt='citipati' w={16} opacity={0.13} /> */}
     </Flex>
   );
 };
 
-const DateTimeDisplay = ({ value, type, color }) => {
+const DateTimeDisplay = ({ value, type }) => {
   return (
-    <Box textAlign='center'>
-      <Text color={color}>{value}</Text>
-      <Text color={color}>{type}</Text>
+    <Box textAlign="center" display={"flex"} gap={2}>
+      <Text fontWeight={"bold"}>{value}</Text>
+      <Text>{type}</Text>
     </Box>
   );
 };
