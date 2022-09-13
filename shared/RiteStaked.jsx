ab3ff598e1d4bfbc9b29e206f1c042b822612624
Marcel <marshby@gmail.com>
2022-09-13 11:32:14 -0500

    Sponsor Bug fixes

--- shared/RiteStaked.jsx
+++ shared/RiteStaked.jsx
@@ -12,8 +12,6 @@ export const RiteStaked = ({
   context,
   raidBalance,
   allowance,
-  isChecked,
-  handleIsChecked,
   cohortAddress,
   handleCohortAddress,
   isApproveTxPending,
@@ -64,15 +62,14 @@ export const RiteStaked = ({
       </Flex>
       {displaySponsorCohort ? (
         <StakingFlow
+          handleSponsorCohort={handleSponsorCohort}
           minimumStake={minimumStake}
           context={context}
           raidBalance={raidBalance}
           allowance={allowance}
-          isChecked={isChecked}
           displaySponsorCohort={displaySponsorCohort}
           checkboxDisplay={displaySponsorCohort ? "none" : null}
           sponsorCohortTextDisplay={displaySponsorCohort ? "none" : null}
-          handleIsChecked={handleIsChecked}
           cohortAddress={cohortAddress}
           handleCohortAddress={handleCohortAddress}
           isApproveTxPending={isApproveTxPending}
